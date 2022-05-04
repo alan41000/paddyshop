82c21d4a1cabfdee7698b10f6a98a64ea1299de8
alan <sing2016@gmail.com>
2022-05-05 01:23:12 +0800

    优化登录逻辑，处理unionid

--- app/common/model/User.php
+++ app/common/model/User.php
@@ -13,47 +13,47 @@ namespace app\common\model;
 
 class User extends PaddyShop
 {
-    protected $type = [
-        'birthday'    =>  'timestamp',
-    ];
+	protected $type = [
+		'birthday'    =>  'timestamp',
+	];
 
-    /**
-     * 用户名登录
-     * @Author: Alan Leung
-     * @param {*} $params
-     */
-    public static function usernameLogin($params)
-    {
-        $where = ['username' => $params['username']];
-        $user = User::where($where)->field('id,username,password,salt,status,is_delete_time')->find();
+	/**
+	 * 用户名登录
+	 * @Author: Alan Leung
+	 * @param {*} $params
+	 */
+	public static function usernameLogin($params)
+	{
+		$where = ['username' => $params['username']];
+		$user = User::where($where)->field('id,username,password,salt,status,is_delete_time')->find();
 
-        if(empty($user))
-        {
-            return app('JsonOutput')->fail('账户不存在');
-        }
+		if(empty($user))
+		{
+			return app('JsonOutput')->fail('账户不存在');
+		}
 
-        $password = loginPwdEncryption($params['password'], $user['salt']);
-        if($password != $user['password'])
-        {
-            return app('JsonOutput')->fail('密码错误');
-        }
+		$password = loginPwdEncryption($params['password'], $user['salt']);
+		if($password != $user['password'])
+		{
+			return app('JsonOutput')->fail('密码错误');
+		}
 
-        // 重写数据
-        $salt = getNumberCode(6);
-        $token = createToken($user['id']);
-        $data = [
-            'id'            =>  $user['id'],
-            'salt'          =>  $salt,
-            'password'      =>  loginPwdEncryption($params['password'], $salt),
-            'token'         =>  $token,
-            'token_expire'  =>  time() + 86400, //1天有效期
-            'last_login'    =>  time(),
-            'last_ip'       =>  '',
-        ];
+		// 重写数据
+		$salt = getNumberCode(6);
+		$token = createToken($user['id']);
+		$data = [
+			'id'            =>  $user['id'],
+			'salt'          =>  $salt,
+			'password'      =>  loginPwdEncryption($params['password'], $salt),
+			'token'         =>  $token,
+			'token_expire'  =>  time() + 86400, //1天有效期
+			'last_login'    =>  time(),
+			'last_ip'       =>  '',
+		];
 
-        if(self::edit($data))
-        {
-            $user = User::where($where)->field('
+		if(self::edit($data))
+		{
+			$user = User::where($where)->field('
             id,
             username,
             nickname,
@@ -80,132 +80,148 @@ class User extends PaddyShop
             openid_qq,
             unionid_qq
             ')->find();
-            return app('JsonOutput')->success($user);
-        }
-
-        return app('JsonOutput')->fail('系统开了个小差，请稍后再试！');
-    }
-
-    public static function wechatMiniAppAuth($params)
-    {
-        // 查询会员是否已注册
-        $userInfo = User::where(['openid_weixin'=>$params['user_base_data']['openid']])->find();
-        if(empty($userInfo))
-        {
-            $user_data = [
-                'openid_alipay'         => '',
-                'openid_weixin'         => $params['user_base_data']['openid'],
-                'unionid_weixin'        => $params['user_base_data']['unionid'] ?? '',
-                'openid_weixin_web'     => '',
-                'openid_baidu'          => '',
-                'openid_toutiao'        => '',
-                'openid_qq'             => '',
-                'unionid_qq'            => '',
-                'status'                => 0,
-                'salt'                  => '',
-                'password'              => '',
-                'token'                 => createToken(time()),
-                'token_expire'          => time() + 86400 * 30,
-                'username'              => '',
-                'nickname'              => $params['nickName'],
-                'mobile'                => '',
-                'email'                 => '',
-                'gender'                => $params['gender'],
-                'avatar'                => $params['avatarUrl'],
-                'province'              => $params['province'],
-                'city'                  => $params['city'],
-                'birthday'              => 0,
-                'address'               => '',
-                'integral'              => 0,
-                'locking_integral'      => 0,
-                'referrer'              => '',
-                'is_delete_time'        => 0,
-                'last_login'            => time(),
-                'last_ip'               => getRealIP(),
-            ];
-
-            $userInfo = self::add($user_data);
-            if($userInfo && config()['paddyshop']['wallet_enable'] == '1'){
-                Wallet::createWalletIfNotExist($userInfo['id']);
-            }
-            return $userInfo;
-        }
-        else
-        {
-            if(config()['paddyshop']['wallet_enable'] == '1'){
-                Wallet::createWalletIfNotExist($userInfo['id']);
-            }            
-            $user_data = [
-                'id'                    => $userInfo['id'],
-                'token'                 => createToken(time()),
-                'token_expire'          => time() + 86400 * 30,
-                'nickname'              => $params['nickName'],
-                'gender'                => $params['gender'],
-                'avatar'                => $params['avatarUrl'],
-                'province'              => $params['province'],
-                'city'                  => $params['city'],
-                'last_login'            => time(),
-                'last_ip'               => getRealIP(),
-            ];
-
-            if(self::edit($user_data))
-            {
-                return User::where(['openid_weixin'=>$params['user_base_data']['openid']])->find();
-            }
-            throwException('系统错误');
-        }
-    }
-
-	public static function wechatH5Auth($params)
-	{
-		// 查询会员是否已注册
-		$userInfo = User::where(['openid_weixin_web'=>$params['raw']['openid']])->find();
-		if(empty($userInfo))
-		{
-			$user_data = [
-				'unionid_weixin'        => $params['raw']['unionid'] ?? '',
-				'openid_weixin_web'     => $params['raw']['openid'],
-				'token'                 => createToken(time()),
-				'token_expire'          => time() + 86400 * 30,
-				'nickname'              => $params['raw']['nickname'],
-				'gender'                => $params['raw']['sex'],
-				'avatar'                => $params['raw']['headimgurl'],
-				'province'              => $params['raw']['province'],
-				'city'                  => $params['raw']['city'],
-				'referrer'              => '',
-				'last_login'            => time(),
-				'last_ip'               => getRealIP(),
-			];
-
-			$userInfo = self::add($user_data);
-			if($userInfo && config()['paddyshop']['wallet_enable'] == '1'){
-				Wallet::createWalletIfNotExist($userInfo['id']);
-			}
-			return $userInfo;
+			return app('JsonOutput')->success($user);
 		}
-		else
+
+		return app('JsonOutput')->fail('系统开了个小差，请稍后再试！');
+	}
+
+	private static  function  userAdd($params,$type)
+	{
+		$user_data = [];
+		switch ($type){
+			case 'wechatMiniApp':
+				$user_data = [
+					'unionid_weixin' => $params['user_base_data']['unionid'] ?? '',
+					'openid_weixin' => $params['user_base_data']['openid'] ?? '',
+					'token'        => createToken(time()),
+					'token_expire' => time() + 86400 * 30,
+					'nickname'     => $params['nickName'],
+					'gender'       => $params['gender'],
+					'avatar'       => $params['avatarUrl'],
+					'province'     => $params['province'],
+					'city'         => $params['city'],
+					'last_login'   => time(),
+					'last_ip'      => getRealIP(),
+				];
+				break;
+
+			case 'wechatH5':
+				$user_data = [
+					'unionid_weixin' => $params['raw']['unionid'] ?? '',
+					'openid_weixin_web' => $params['raw']['openid'],
+					'token'          => createToken(time()),
+					'token_expire'   => time() + 86400 * 30,
+					'nickname'       => $params['raw']['nickname'],
+					'gender'         => $params['raw']['sex'],
+					'avatar'         => $params['raw']['headimgurl'],
+					'province'       => $params['raw']['province'],
+					'city'           => $params['raw']['city'],
+					'last_login'     => time(),
+					'last_ip'      => getRealIP(),
+				];
+				break;
+
+			default:
+				throwException('系统开了个小差，请稍后再试！');
+		}
+
+		$userInfo = self::add($user_data);
+		if($userInfo && config()['paddyshop']['wallet_enable'] == '1'){
+			Wallet::createWalletIfNotExist($userInfo['id']);
+		}
+		return $userInfo;
+	}
+
+	private static  function  userUpdate($params,$userInfo,$type)
+	{
+		if(config()['paddyshop']['wallet_enable'] == '1'){
+			Wallet::createWalletIfNotExist($userInfo['id']);
+		}
+
+		$user_data = [];
+		switch ($type){
+			case 'wechatMiniApp':
+				$user_data = [
+					'id'           => $userInfo['id'],
+					'unionid_weixin' => $params['user_base_data']['unionid'] ?? '',
+					'openid_weixin' => $params['user_base_data']['openid'] ?? '',
+					'token'        => createToken(time()),
+					'token_expire' => time() + 86400 * 30,
+					'nickname'     => $params['nickName'],
+					'gender'       => $params['gender'],
+					'avatar'       => $params['avatarUrl'],
+					'province'     => $params['province'],
+					'city'         => $params['city'],
+					'last_login'   => time(),
+					'last_ip'      => getRealIP(),
+				];
+				break;
+
+			case 'wechatH5':
+				$user_data = [
+					'id'             => $userInfo['id'],
+					'unionid_weixin' => $params['raw']['unionid'] ?? '',
+					'openid_weixin_web' => $params['raw']['openid'] ?? '',
+					'token'          => createToken(time()),
+					'token_expire'   => time() + 86400 * 30,
+					'nickname'       => $params['raw']['nickname'],
+					'gender'         => $params['raw']['sex'],
+					'avatar'         => $params['raw']['headimgurl'],
+					'province'       => $params['raw']['province'],
+					'city'           => $params['raw']['city'],
+					'last_login'     => time(),
+				];
+				break;
+
+			default:
+				throwException('系统开了个小差，请稍后再试！');
+		}
+
+		if(self::edit($user_data))
 		{
+			if($type == 'wechatMiniApp'){
+				return User::where(['openid_weixin'=>$params['user_base_data']['openid']])->find();
+			}
+			if($type == 'wechatH5'){
+				return User::where(['openid_weixin_web'=>$params['raw']['openid']])->find();
+			}
+		}
+		throwException('系统开了个小差，请稍后再试！');
+	}
+
+	public static function wechatMiniAppAuth($params)
+	{
+		$unionid = $params['user_base_data']['unionid'] ?? '';
+		if(!empty($unionid)){
+			$userInfo = User::where(['unionid_weixin'=>$unionid])->find();
 			if(config()['paddyshop']['wallet_enable'] == '1'){
 				Wallet::createWalletIfNotExist($userInfo['id']);
 			}
-			$user_data = [
-				'id'                    => $userInfo['id'],
-				'unionid_weixin'        => $params['raw']['unionid'] ?? '',
-				'token'                 => createToken(time()),
-				'token_expire'          => time() + 86400 * 30,
-				'nickname'              => $params['raw']['nickname'],
-				'gender'                => $params['raw']['sex'],
-				'avatar'                => $params['raw']['headimgurl'],
-				'province'              => $params['raw']['province'],
-				'city'                  => $params['raw']['city'],
-				'last_login'            => time(),
-			];
-
-			if(self::edit($user_data))
-			{
-				return User::where(['openid_weixin_web'=>$params['raw']['openid']])->find();
+			return self::userUpdate($params,$userInfo,'wechatMiniApp');
+		}else{
+			$userInfo = User::where(['openid_weixin'=>$params['user_base_data']['openid']])->find();
+			if(empty($userInfo)){
+				return self::userAdd($params,'wechatMiniApp');
+			}else{
+				return self::userUpdate($params,$userInfo,'wechatMiniApp');
 			}
-			throwException('系统错误');
+		}
+	}
+
+	public static function wechatH5Auth($params)
+	{
+		$unionid = $params['raw']['unionid'] ?? '';
+		if(!empty($unionid)){
+			$userInfo = User::where(['unionid_weixin'=>$unionid])->find();
+			return self::userUpdate($params,$userInfo,'wechatH5');
+		}else{
+			$userInfo = User::where(['openid_weixin_web'=>$params['raw']['openid']])->find();
+			if(empty($userInfo)){
+				return self::userAdd($params,'wechatH5');
+			}else{
+				return self::userUpdate($params,$userInfo,'wechatH5');
+			}
 		}
 	}
 }
